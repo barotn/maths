1d3ce027ba362446382a63550bdc656b4112daa5
Nilesh Barot <nilesh.barot@itv.com>
2013-11-01 09:18:55 +0000

    Adding index.php index.css and updating dmas.php

--- dmas.php
+++ dmas.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html><title>
-Addition, Subtraction, Multiplication SUMS</title>
+Addition, Subtraction, Multiplication, Division SUMS</title>
 <head>
 	<style type="text/css">
 		
@@ -22,14 +22,14 @@ Addition, Subtraction, Multiplication SUMS</title>
 <body>
 	<div class="twodigits">
 
-		<p>Two digits Addition, Subtraction, Multiplication sums.</p>
+		<p>Two digits Addition, Subtraction, Multiplication and Division sums.</p>
 		<ol>
 			<li><a href="./2digits/addition_2_digit.php" target="_blank">Two digit Addition</a></li>
 			<li><a href="./2digits/subtraction_2_digit.php" target="_blank">Two digit Subtraction</a></li>
 			<li><a href="./2digits/multiplication_2_digit.php" target="_blank">Two digit Multiplication</a></li>
 			<li><a href="./2digits/division_2_digit.php" target="_blank">Two digit Division</a></li>
 		</ol>
-		<p>Three digits Addition, Subtraction, Multiplication sums.</p>
+		<p>Three digits Addition, Subtraction, Multiplication and Division sums.</p>
 		<ol>
 			<li><a href="./3digits/addition_3_digit.php" target="_blank">Three digit Addition</a></li>
 			<li><a href="./3digits/subtraction_3_digit.php" target="_blank">Three digit Subtraction</a></li>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+<link type="text/css" rel="stylesheet" href="index.css"/>
+</head>
+<body>
+<!-- <header>
+	<nav>
+		<ul id="nav-list">
+			<li><a href="#" title="Home">Home</a></li>
+			<li><a href="#" title="About">About</a></li>
+		</ul>
+	</nav>
+</header> -->
+<h2 class="welcome"><a href="http://www.123learn.org">Welcome to www.123learn.org</a></h2>
+<div class="tab1">
+	<h1><a href="./dmas.php" target="_blank">Maths Worksheets</a></h1>
+</div>
+</body>
+</html>
